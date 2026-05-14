04b11a2a58bd60d504f39aedc4af59f4f9620206
Daniel <gildresdaniel1@gmail.com>
2026-05-14 11:53:41 +0200

    Linq

--- Program.cs
+++ Program.cs
@@ -1,92 +1,9 @@
-﻿using System.ComponentModel.Design;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.ConstrainedExecution;
-using System.Security.Cryptography;
-
+﻿
 internal class Program
 {
     private static void Main(string[] args)
     {
-        // List<int> numeros = new List<int> {50, 55, 78, 90 , 100, 1, 20};
-
-        // var pasan = from numero in numeros
-        //             where numero > 50
-        //             orderby numero ascending
-        //             select numero;
-
-        // foreach (var numero in pasan)
-        // {
-        //     System.Console.WriteLine(numero);
-        // }
-
-        // List<string> nombres = new List<string> {"Daniel", "Emanuel", "Messi", "Cris", "Ada" };
-
-        // var found = from nombre in nombres
-        //             where (nombre.Contains('e'))
-        //             select nombre;
-
-        // foreach (var nombre in found)
-        // {
-        //     System.Console.WriteLine(nombre);
-        // }
-
-        // List<string> nombres2 = new List<string> {"Ada", "Guillermo", "Cris", "Lucas", "Roc"};
-
-        // var largo = from nombre1 in nombres2
-        //             orderby nombre1.Length
-        //             select nombre1;
-
-        // foreach (var nombre1 in largo)
-        // {
-        //     System.Console.WriteLine(nombre1);
-        // }
-
-        // System.Console.WriteLine("---------------------------------");
-
-
-
-        // List<Productos> productos = new List<Productos>
-        // {
-        //     new Productos {Nombre = "Lapiz", Precio = 21},
-        //     new Productos {Nombre = "Pen", Precio = 30},
-        //     new Productos {Nombre = "Libro", Precio = 100},
-        //     new Productos {Nombre = "Mochila", Precio = 105},
-        //     new Productos {Nombre = "Juego", Precio = 44},
-
-        // };
-
-
-        // var orden = from producto in productos
-        //             where producto.Precio < 100
-        //             orderby producto.Precio ascending
-        //             select producto;
-
-        // foreach (var Productos in orden)
-        // {
-        //     System.Console.WriteLine(Productos);
-        // }
-
-
-
-
-        // List<Empleado> empleados = new List<Empleado>
-        // {
-        //     new Empleado{ Nombre = "Ana"}
-        // };
-
-
-
-
-        // var resultado = from empleado in empleados
-        //                 where empleado.Experiencia > 5
-        //                 orderby empleado.Experiencia descending
-        //                 select empleado;
-
-        // foreach (var item in empleados)
-        // {
-        //     System.Console.WriteLine(item);
-        // }
-
+        
 
         List<Empleado> empleados = new List<Empleado>
     {
@@ -114,11 +31,42 @@ internal class Program
                     
     System.Console.WriteLine(promedio);
 
-                        
 
-}
+     // Ordena la lista de empleados por departamento en orden ascendente
+    var ordenados = from empleado in empleados
+                    orderby empleado.Departamento ascending
+                    select empleado;
+    
+    foreach (var empleado in ordenados)
+    {
+        System.Console.WriteLine($"{empleado.Nombre}, {empleado.Departamento}");
+    }
+
+    // Selecciona el empleado de mayor edad
+    var empleadoMayorEdad = (from empleado in empleados
+                            orderby empleado.Edad descending
+                            select empleado).FirstOrDefault();
+
+    System.Console.WriteLine(empleadoMayorEdad.Nombre);
+
+    // Selecciona el primer empleado que tenga más de 30 años
+    var empleadoMayor30 = (from empleado in empleados
+                            where empleado.Edad > 30
+                            select empleado).First();
+
+    System.Console.WriteLine(empleadoMayor30.Nombre);
 
 
+    // Selecciona los 2 primeros empleados
+    var dosPrimeros = (from empleado in empleados
+                       select empleado).Take(2);
+
+    foreach (var empleado in dosPrimeros)
+    {
+        System.Console.WriteLine(empleado.Nombre);
+    }
+
+    }
 
     public class Empleado
     {
